45b1b35e9d4694c1db9ed862f953dac3d3247b35
anhle150199 <anhle150199@gmail.com>
2020-12-01 23:36:47 +0700

    add + edit template

---
+++ resources/views/layouts/elements/footer.blade.php
@@ -0,0 +1,102 @@
+    <!-- ##### Footer Area Start ##### -->
+    <footer class="footer-area">
+        <div class="container">
+            <div class="row">
+                <!-- Footer Widget Area -->
+                <div class="col-12 col-sm-6 col-xl-3">
+                    <div class="footer-widget mb-70" style="margin-top: 10%;">
+                        <!-- Logo -->
+                        <a href="home" class="foo-logo d-block mb-4"><img src="<?php echo url('/'); ?>/img/core-img/logo1.png" alt=""></a>
+                        <p></p>
+
+                    </div>
+                </div>
+
+                <!-- Footer Widget Area -->
+                <div class="col-12 col-sm-6 col-xl-3">
+                    <div class="footer-widget mb-70">
+                        <div class="twitter-slides owl-carousel">
+                            <div class="">
+                                <div class="single-twit">
+                                    <p>Chúng tôi là một trang web phi lợi nhuận, giúp mọi người có hứng thú với lịch sử nước nhà nhiều hơn. Nếu bạn cảm thấy lịch sử nhàm chán, hãy đến với chúng tôi</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Footer Widget Area -->
+                <div class="col-12 col-sm-6 col-xl-3">
+                    <div class="footer-widget mb-70">
+                        <h6 class="widget-title">Các trang thông tin</h6>
+                        <div class="post-content">
+                            <a href="http://quochoi.vn/Pages/default.aspx" class="post-title">- Quốc hội VN</a>
+                        </div>
+                        <br>
+                        <div class="post-content">
+                            <a href="https://dangcongsan.vn/" class="post-title">- Đảng Cộng Sản VN</a>
+                        </div>
+                        <br>
+                        <div class="post-content">
+                            <a href="http://www.chinhphu.vn/portal/page/portal/chinhphu/trangchu" class="post-title">- Chính Phủ VN</a>
+                        </div>
+                        <br>
+                        <div class="post-content">
+                            <a href="https://daibieunhandan.vn/" class="post-title">- Đại biểu nhân dân</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Footer Widget Area -->
+                <div class="col-12 col-sm-6 col-xl-3">
+                    <div class="footer-widget mb-70">
+                        <h6 class="widget-title">Địa chỉ</h6>
+                        <!-- Contact Address -->
+                        <div class="contact-address">
+                            <p>Số 1, Đại Cồ Việt <br>Hai Bà Trưng, Hà Nội</p>
+                            <p>Điện thoại: 0386435002</p>
+                            <p>Email: lichsuvietnam@gmail.com</p>
+                        </div>
+                        <!-- Footer Social Area -->
+                        <div class="footer-social-area">
+                            <a href="https://www.facebook.com/lichsunuocvietnam" class="facebook"><i class="fa fa-facebook"></i></a>
+                            <a href="https://www.google.com" class="google-plus"><i class="fa fa-google-plus"></i></a>
+                            <a href="https://www.instagram.com/" class="instagram"><i class="fa fa-instagram"></i></a>
+                            <a href="https://twitter.com" class="twitter"><i class="fa fa-twitter"></i></a>
+                            <a href="https://www.linkedin.com/" class="linkedin"><i class="fa fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Copywrite Area -->
+        <div class="copywrite-area">
+            <div class="container">
+                <div class="row align-items-center">
+                    <!-- Copywrite Text -->
+                    <div class="col-12 col-sm-6">
+                        <p class="copywrite-text">
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            Copyright &copy;<script>
+                                document.write(new Date().getFullYear());
+                            </script> All rights reserved 
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        </p>
+                    </div>
+                    <div class="col-12 col-sm-6">
+                        <!-- <nav class="footer-nav">
+                            <ul>
+                                <li><a href="#">Advertise</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Contact</a></li>
+                                <li><a href="#">Disclaimer</a></li>
+                                <li><a href="#">Privacy</a></li>
+                            </ul>
+                        </nav> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- ##### Footer Area End ##### -->
---
+++ resources/views/layouts/elements/header.blade.php
@@ -0,0 +1,139 @@
+
+    <!-- ##### Header Area Start ##### -->
+    <header class="header-area">
+        <!-- Top Header Area -->
+        <div class="top-header-area">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-12 col-md-6">
+                        <!-- Breaking News Widget -->
+                        <div class="breaking-news-area d-flex align-items-center">
+                            <div class="news-title">
+                                <!-- <p>Breaking News:</p> -->
+                            </div>
+                            <div id="breakingNewsTicker" class="ticker">
+                                <!-- <ul>
+                                    <li><a href="single-post.html">10 Things Amazon Echo Can Do</a></li>
+                                    <li><a href="single-post.html">Welcome to Colorlib Family.</a></li>
+                                    <li><a href="single-post.html">Boys 'doing well' after Thai</a></li> -->
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="top-meta-data d-flex align-items-center justify-content-end">
+                            <!-- Top Social Info -->
+                            <div class="top-social-info">
+                                <a href="https://www.facebook.com/lichsunuocvietnam"><i class="fa fa-facebook"></i></a>
+                                <a href="https://twitter.com/"><i class="fa fa-twitter"></i></a>
+                                <a href="https://www.pinterest.com/"><i class="fa fa-pinterest"></i></a>
+                                <a href="https://www.linkedin.com/"><i class="fa fa-linkedin"></i></a>
+                                <a href="https://www.youtube.com/"><i class="fa fa-youtube-play"></i></a>
+                            </div>
+                            <!-- Top Search Area -->
+                            <div class="top-search-area">
+                                <form action="index.html" method="post">
+                                    <input type="search" name="top-search" id="topSearch" placeholder="Search...">
+                                    <button type="submit" class="btn"><i class="fa fa-search" aria-hidden="true"></i></button>
+                                </form>
+                            </div>
+                            <!-- Login -->
+                            <a href="login" class="login-btn"><i class="fa fa-user" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Navbar Area -->
+        <div class="vizew-main-menu" id="sticker">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="vizewNav">
+
+                        <!-- Nav brand -->
+                        <a href="home" class="nav-brand" style="max-width: 300px;"><img src="<?php echo url('/'); ?>/img/core-img/logo1.png" alt=""></a>
+
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+
+                        <div class="classy-menu">
+
+                            <!-- Close Button -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+
+                            <!-- Nav Start -->
+                            <div class="classynav">
+                                <ul>
+                                    <li class="active"><a href="home">Trang Chủ</a></li>
+                                    <li><a href="danh-nhan">Danh nhân</a></li>
+                                    <li><a href="#">Sự Kiện</a>
+                                        <ul class="dropdown" style="width: max-content;">
+                                            <li><a href="index.html">- Thời cổ đại</a></li>
+                                            <li><a href="archive-list.html">- Thời Trung Đại</a></li>
+                                            <li><a href="archive-grid.html">- Thời cận đại(1858-1945)</a></li>
+                                            <li><a href="single-post.html">- Thời hiện đại</a></li>
+                                        </ul>
+                                    </li>
+                                    <!-- <li><a href="#">Di tích</a>
+                                        <div class="megamenu">
+                                            <ul class="single-mega cn-col-4">
+                                                <li><a href="index.html">- Home</a></li>
+                                                <li><a href="archive-list.html">- Archive List</a></li>
+                                                <li><a href="archive-grid.html">- Archive Grid</a></li>
+                                                <li><a href="single-post.html">- Single Post</a></li>
+                                                <li><a href="video-post.html">- Single Video Post</a></li>
+                                                <li><a href="contact.html">- Contact</a></li>
+                                                <li><a href="typography.html">- Typography</a></li>
+                                                <li><a href="login.html">- Login</a></li>
+                                            </ul>
+                                            <ul class="single-mega cn-col-4">
+                                                <li><a href="index.html">- Home</a></li>
+                                                <li><a href="archive-list.html">- Archive List</a></li>
+                                                <li><a href="archive-grid.html">- Archive Grid</a></li>
+                                                <li><a href="single-post.html">- Single Post</a></li>
+                                                <li><a href="video-post.html">- Single Video Post</a></li>
+                                                <li><a href="contact.html">- Contact</a></li>
+                                                <li><a href="typography.html">- Typography</a></li>
+                                                <li><a href="login.html">- Login</a></li>
+                                            </ul>
+                                            <ul class="single-mega cn-col-4">
+                                                <li><a href="index.html">- Home</a></li>
+                                                <li><a href="archive-list.html">- Archive List</a></li>
+                                                <li><a href="archive-grid.html">- Archive Grid</a></li>
+                                                <li><a href="single-post.html">- Single Post</a></li>
+                                                <li><a href="video-post.html">- Single Video Post</a></li>
+                                                <li><a href="contact.html">- Contact</a></li>
+                                                <li><a href="typography.html">- Typography</a></li>
+                                                <li><a href="login.html">- Login</a></li>
+                                            </ul>
+                                            <ul class="single-mega cn-col-4">
+                                                <li><a href="index.html">- Home</a></li>
+                                                <li><a href="archive-list.html">- Archive List</a></li>
+                                                <li><a href="archive-grid.html">- Archive Grid</a></li>
+                                                <li><a href="single-post.html">- Single Post</a></li>
+                                                <li><a href="video-post.html">- Single Video Post</a></li>
+                                                <li><a href="contact.html">- Contact</a></li>
+                                                <li><a href="typography.html">- Typography</a></li>
+                                                <li><a href="login.html">- Login</a></li>
+                                            </ul>
+                                        </div>
+                                    </li> -->
+                                    <li><a href="contact.html">Di Tích</a></li>
+                                    <li><a href="contact.html">Video</a></li>
+                                </ul>
+                            </div>
+                            <!-- Nav End -->
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ##### Header Area End ##### -->
